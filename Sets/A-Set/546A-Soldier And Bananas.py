a038a65cd12f33745a7ea0ac43736615fa520db6
Matthxsmp <matthxsmp@gmail.com>
2026-04-30 02:40:15 -0500

    546A
    Soldier and Bananas

--- Sets/A-Set/546A-Soldier And Bananas.py
+++ Sets/A-Set/546A-Soldier And Bananas.py
@@ -1,13 +1,20 @@
 import sys
 input = sys.stdin.readline
 def main():
-    b, i, q = map(int,input().strip().split())
-    r=b
-    while q>0:
-        q-=1
-        i-=b
-        b+=r
-    print(abs(i))
+    A, B, C = map(int,input().strip().split())
+    D = 0
+    
+    for i in range (C):
+        D += A * (i+1)
+    
+    B -= D
+    if B < 0 :
+        print(abs(B))
+    elif B > 0:
+        print(0)
+    else :
+        print (0)
+    
 main()
 
 
